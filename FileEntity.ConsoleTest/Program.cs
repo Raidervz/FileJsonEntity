17999c259a4f37ec78eb73ce33993f1a5862baad
Ramon Rodriguez <ramon.aris.rodriguez@outlook.com>
2020-05-09 04:34:14 -0500

    Added Solution and Segregation of Projects

---
+++ FileEntity.ConsoleTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileEntity.ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ FileEntity.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FileEntity.Core
+{
+    public class Class1
+    {
+    }
+}
